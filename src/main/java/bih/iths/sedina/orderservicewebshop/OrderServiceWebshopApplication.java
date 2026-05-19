317df2e87a8e845c7dd84dad4a3224e046e008c7
sedina-imamovic <Sedina.Imamovic@iths.se>
2026-05-19 11:57:07 +0200

    initial commit

---
+++ src/main/java/bih/iths/sedina/orderservicewebshop/OrderServiceWebshopApplication.java
@@ -0,0 +1,13 @@
+package bih.iths.sedina.orderservicewebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceWebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceWebshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bih/iths/sedina/orderservicewebshop/OrderServiceWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package bih.iths.sedina.orderservicewebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceWebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
