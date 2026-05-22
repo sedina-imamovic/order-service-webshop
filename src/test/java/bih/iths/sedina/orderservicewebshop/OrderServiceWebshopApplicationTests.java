a6eca97523b038707938c7ba1f0ffe88258da91e
sedina-imamovic <Sedina.Imamovic@iths.se>
2026-05-22 13:41:57 +0200

    initial commit

--- src/test/java/bih/iths/sedina/orderservicewebshop/OrderServiceWebshopApplicationTests.java
+++ src/test/java/bih/iths/sedina/orderservicewebshop/OrderServiceWebshopApplicationTests.java
@@ -1,10 +1,19 @@
 package bih.iths.sedina.orderservicewebshop;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class OrderServiceWebshopApplicationTests {
+
+    @MockitoBean
+    private RabbitTemplate rabbitTemplate;
+
+    @MockitoBean
+    private JwtDecoder jwtDecoder;
 
     @Test
     void contextLoads() {
