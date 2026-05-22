74ac0db86aac2b3e5d99c704a3a317a604bf6a96
sedina-imamovic <Sedina.Imamovic@iths.se>
2026-05-22 14:20:10 +0200

    initial commit

--- src/main/java/bih/iths/sedina/orderservicewebshop/model/OrderItem.java
+++ src/main/java/bih/iths/sedina/orderservicewebshop/model/OrderItem.java
@@ -1,9 +1,6 @@
 package bih.iths.sedina.orderservicewebshop.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,5 +18,9 @@ public class OrderItem {
     private Double price;
 
     private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
 
 }
--- src/main/java/bih/iths/sedina/orderservicewebshop/publisher/MessagePublisher.java
+++ src/main/java/bih/iths/sedina/orderservicewebshop/publisher/MessagePublisher.java
@@ -1,9 +1,13 @@
 package bih.iths.sedina.orderservicewebshop.publisher;
 
 import bih.iths.sedina.orderservicewebshop.config.RabbitConfig;
+import bih.iths.sedina.orderservicewebshop.model.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -11,7 +15,25 @@ public class MessagePublisher {
 
     private final RabbitTemplate rabbitTemplate;
 
-    public void publish(String message) {
+    public void publish(Order order, String email) {
+
+        Map<String, Object> message = new HashMap<>();
+
+        message.put("orderId", order.getId());
+        message.put("email", email);
+        message.put("customerName", order.getCustomerName());
+        message.put("orderDate", order.getOrderDate());
+        message.put("totalPrice", order.getTotalPrice());
+
+        message.put("items",
+                order.getOrderItems().stream()
+                        .map(item -> Map.of(
+                                "productId", item.getId(),
+                                "name", item.getName(),
+                                "price", item.getPrice(),
+                                "quantity", item.getQuantity()
+                        ))
+                        .toList());
 
         rabbitTemplate.convertAndSend(RabbitConfig.QUEUE, message);
     }
