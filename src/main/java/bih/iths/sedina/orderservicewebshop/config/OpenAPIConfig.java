045c86044eaae96eb24a5a2a2c19f8fc42f1001d
sedina-imamovic <Sedina.Imamovic@iths.se>
2026-06-02 14:46:41 +0200

    initial commit

---
+++ src/main/java/bih/iths/sedina/orderservicewebshop/config/OpenAPIConfig.java
@@ -0,0 +1,29 @@
+package bih.iths.sedina.orderservicewebshop.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        String schemeName = "bearerAuth";
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Order Service API")
+                        .description("Simple JWT protected order-service")
+                        .version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList(schemeName))
+                .components(new Components()
+                        .addSecuritySchemes(schemeName, new SecurityScheme()
+                                .name(schemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
