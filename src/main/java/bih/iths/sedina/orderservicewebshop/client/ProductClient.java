ae2edc9b500127b2d8a56167b697b8ba18a750b1
sedina-imamovic <Sedina.Imamovic@iths.se>
2026-06-03 22:54:55 +0200

    initial commit

--- src/main/java/bih/iths/sedina/orderservicewebshop/client/ProductClient.java
+++ src/main/java/bih/iths/sedina/orderservicewebshop/client/ProductClient.java
@@ -19,7 +19,7 @@ public class ProductClient {
                                            String bearerToken) {
         return restClient.post()
                 .uri("/products/stock/decrease")
-                .header("Authorization", bearerToken)
+                .header("Authorization", "Bearer" + bearerToken)
                 .body(items)
                 .retrieve()
                 .body(new ParameterizedTypeReference<>() {
